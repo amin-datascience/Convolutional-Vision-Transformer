6a430e6a0386b659211264067e785668b4993952
Amin Beigli <amin.beigli22@gmail.com>
2023-01-06 21:45:20 +0330

    Added parser

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
 from timm.scheduler import create_scheduler
 import matplotlib.pyplot as plt  
 
+import argparse
 from utils import clip_gradient
 import warmup_scheduler
 import os 
@@ -156,6 +157,37 @@ def train_func(train_loader, model, optimizer, loss_func, max_epochs = 100,
             'val_loss': validation_loss, 'val_accuracy': validation_accuracy}
 
 
+def get_args_parser():
+
+    parser = argparse.ArgumentParser('ConViT training and evaluation script', add_help=False)
+    parser.add_argument('--sched', default='cosine', type=str, metavar='SCHEDULER',
+                        help='LR scheduler (default: "cosine"')
+    parser.add_argument('--lr', type=float, default=5e-4, metavar='LR',
+                        help='learning rate (default: 5e-4)')
+    parser.add_argument('--lr-noise', type=float, nargs='+', default=None, metavar='pct, pct',
+                        help='learning rate noise on/off epoch percentages')
+    parser.add_argument('--lr-noise-pct', type=float, default=0.67, metavar='PERCENT',
+                        help='learning rate noise limit percent (default: 0.67)')
+    parser.add_argument('--lr-noise-std', type=float, default=1.0, metavar='STDDEV',
+                        help='learning rate noise std-dev (default: 1.0)')
+    parser.add_argument('--warmup-lr', type=float, default=1e-6, metavar='LR',
+                        help='warmup learning rate (default: 1e-6)')
+    parser.add_argument('--min-lr', type=float, default=1e-5, metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0 (1e-5)')
+
+    parser.add_argument('--decay-epochs', type=float, default=30, metavar='N',
+                        help='epoch interval to decay LR')
+    parser.add_argument('--warmup-epochs', type=int, default=5, metavar='N',
+                        help='epochs to warmup LR, if scheduler supports')
+    parser.add_argument('--cooldown-epochs', type=int, default=10, metavar='N',
+                        help='epochs to cooldown LR at min_lr, after cyclic schedule ends')
+    parser.add_argument('--patience-epochs', type=int, default=10, metavar='N',
+                        help='patience epochs for Plateau LR scheduler (default: 10')
+    parser.add_argument('--decay-rate', '--dr', type=float, default=0.1, metavar='RATE',
+                        help='LR decay rate (default: 0.1)')
+
+    return parser
+
 
 def main(parameters):
 
@@ -202,11 +234,14 @@ def main(parameters):
         criterion = SoftTargetCrossEntropy()
         val_criterion = nn.CrossEntropyLoss().to(device)
 
+    #Adding parser
+    parser = argparse.ArgumentParser('ConViT training and evaluation script', parents=[get_args_parser()])
+    args = parser.parse_args()
+
     optimizer = torch.optim.AdamW(model.parameters(), lr = parameters['lr'], weight_decay = parameters['weight_decay'])
     #base_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = 100, eta_min = 1e-6)
     #scheduler = warmup_scheduler.GradualWarmupScheduler(optimizer, multiplier=1., total_epoch=5, after_scheduler = base_scheduler)
-    scheduler, _ = create_scheduler(optimizer = optimizer, sched = 'cosine', lr = parameters['lr'], warmup_lr = 1e-6, min_lr = 1e-5, 
-        decay_epochs = 30, warmup_epochs = 5, cooldown_epochs = 10, patience_epochs = 10, decay_rate = 0.1)
+    scheduler, _ = create_scheduler(args, optimizer)
 
 
     model, history = train_func(train_loader = train_loader, model = model, 
